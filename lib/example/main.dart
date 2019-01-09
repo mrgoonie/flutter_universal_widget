fdd3dc3db5d04f0144465b14d917c6c4e94424ae
Duy Nguyen (BOSS) <duynguyen@digitop.vn>
2019-01-10 04:24:46 +0700

    fixed example

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -8,9 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
+    return HomeScreen();
   }
 }
 
